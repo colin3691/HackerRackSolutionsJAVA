91a0bfd1e5818df505467ef02ab63098235f6f39
Luis Colin <luisangel12963@gmail.com>
2024-03-21 16:04:02 -0600

    Agregando archivo.txt

---
+++ Mayor/src/com/Principal.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main (String []args) {
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int array[] = new int [5];
+		int mayor = array[0];
+		System.out.println("ingrese los 5 numeros : ");
+		for (int i =0; i<array.length;i++) {
+			
+		array [i]=sc.nextInt();
+		if(array[i]>mayor) {
+			mayor=array[i];
+		}
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		int arrat¨¨¨´´´´¨¨´´ppppp¨¨´´´´´´´´¨¨
+	}
+
+}
---
+++ lecturaDeNumeros/src/com/Principal.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main (String []args) {
+        System.out.println("Ingrese tres numeros  : ");
+
+		Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+         System.out.println(b);
+          System.out.println(c);
+
+		
+		
+	}
+}
